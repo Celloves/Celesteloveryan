64e069389dc5f9921eeeaf919097d7e8572278a0
ni1211 <66164754+ni1211@users.noreply.github.com>
2021-03-03 11:54:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ var config = {
         "早睡早起",
         "像你之前一样早期锻炼",
         "最后，祝你生日快乐！",
-        "小孩最棒！（溜须拍马）",
+        "小孩最棒！（套话，别信）",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -24,7 +24,17 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "送给小孩": "./imgs/AlbertaBubbles_ZH-CN1528424173_1920x1080.jpg",
+        "送给小孩": "./imgs/AlbertaBubbles_ZH-CN1528424173_1920x1080.jpg",
+        "送给小孩": "./imgs/AlbertaBubbles_ZH-CN1528424173_1920x1080.jpg",
+        "送给小孩": "./imgs/AlbertaBubbles_ZH-CN1528424173_1920x1080.jpg",
+        "送给小孩": "./imgs/AlbertaBubbles_ZH-CN1528424173_1920x1080.jpg",
+        "送给小孩": "./imgs/AlbertaBubbles_ZH-CN1528424173_1920x1080.jpg",
+        "送给小孩": "./imgs/AlbertaBubbles_ZH-CN1528424173_1920x1080.jpg",
+        "送给小孩": "./imgs/AlbertaBubbles_ZH-CN1528424173_1920x1080.jpg",
+        "送给小孩": "./imgs/AlbertaBubbles_ZH-CN1528424173_1920x1080.jpg",
+        "送给小孩": "./imgs/AlbertaBubbles_ZH-CN1528424173_1920x1080.jpg",
+        
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
